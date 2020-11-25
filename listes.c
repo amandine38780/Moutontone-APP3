f44ae61f3d10acb2b13893573ae2ec26bf2c4743
moutontone <grenier.amrandpor@gmail.com>
2020-11-25 08:18:29 +0100

    first commit

---
+++ listes.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "listes.h"
+
+/* fichier à compléter au besoin */
+
+void init_liste_vide(liste_t* L) {
+    /* a completer */
+}
+
+
+int ajouter_tete(liste_t* L, string c) { /* retourne 0 si OK, 1 sinon  */
+    /* a completer */
+        return 1;
+}
+
